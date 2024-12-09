256aae6d9e56fcd30f4b6d4e8469aec8b273374d
saaya-code <boualimohamed74@gmail.com>
2024-12-09 21:57:15 +0100

    fix database connectivity and add seed file

--- app/challenges/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ChallengesPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-rosewood to-blackbean overflow-hidden">
-      challenges
-    </div>
-  );
-}
