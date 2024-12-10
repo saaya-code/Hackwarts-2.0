50ed8d6be2bf158bfb9921fe9cdaa4040c715e2b
DalyChouikh <chouikhdaly215@gmail.com>
2024-12-10 12:33:25 +0100

    add: Added multiple protected routes in the authentication middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,24 @@
+import { auth } from "./app/auth";
+import { NextResponse } from "next/server";
 
-import { auth } from "./app/auth"
-import { NextResponse } from "next/server"
- 
 export default auth((req) => {
-  const isAuth = !!req.auth
-  const isProtectedRoute = req.nextUrl.pathname.startsWith('/challenges')
-  
+  const isAuth = !!req.auth;
+  const protectedRoutes = [
+    "/challenges",
+    "/profile",
+    "/create-team",
+    "/submission",
+  ];
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    req.nextUrl.pathname.startsWith(route),
+  );
+
   if (isProtectedRoute && !isAuth) {
-    return NextResponse.redirect(new URL('/register', req.nextUrl))
+    return NextResponse.redirect(new URL("/register", req.nextUrl));
   }
-})
+});
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-}
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
+
